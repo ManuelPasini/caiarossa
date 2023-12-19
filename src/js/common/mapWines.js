8690846c1e77f5d2c034742e535b3dabc5307d04
manuel pasini <mpasini@websolute.it>
2023-12-19 17:35:10 +0100

    affinamenti mappa interattiva

--- src/js/common/mapWines.js
+++ src/js/common/mapWines.js
@@ -1,27 +1,42 @@
 export default function mapWines() {
-
-    // Define elements
     var container = document.getElementById('mapWines');
     var wraptext = document.getElementById('wraptext');
     var innerWraptext = document.getElementById('inner_wraptext');
     var title, text, extratext;
     var defaultData = {};
     var offset = 64;
+    var pin, defaultElement;
 
     async function getData() {
-        // Get data
-        const response = await fetch("/assets/json/mapWines.json");
-        const items = await response.json();
+        let request;
 
-        createPins(items);
+        request = {
+            method: 'GET',
+            cache: 'no-store',
+            headers: {
+                "Content-Type": "application/json",
+            },
+        };
+
+        try {
+            let response = await fetch('/assets/json/mapWines.json', request);
+
+            if (!response.ok) {
+                throw new Error(`HTTP error! Status: ${response.status}`);
+            }
+
+            let responseData = await response.json();
+            createPins(responseData);
+            return responseData;
+        } catch (error) {
+            console.error('Fetch error:', error);
+            throw error;
+        }
     }
 
     function createPins(items) {
-        for (let i = 0; i < items.length; i++) {
-            let item = items[i];
-
-            // Create pin
-            var pin = document.createElement('div');
+        items.forEach((item, i) => {
+            pin = document.createElement('div');
             pin.className = 'label40 mapWines__pin';
             pin.textContent = item.title;
             pin.style.left = item.position.left + '%';
@@ -29,7 +44,6 @@ export default function mapWines() {
 
             container.appendChild(pin);
 
-            // Create default content
             if (i == 0) {
                 pin.classList.add('default', 'active');
                 wraptext.classList.add('wraptext--bg' + item.color);
@@ -58,54 +72,51 @@ export default function mapWines() {
                 defaultData.title = item.title;
                 defaultData.text = item.text;
                 defaultData.extratext = item.extratext;
+                defaultElement = pin;
             }
 
-            // Hover effect + change content
-            var defaultElement = container.querySelector('.default');
-
-            if (pin != defaultElement) {
-                pin.addEventListener('mouseenter', function () {
-
-                    defaultElement.classList.remove("active");
-                    this.classList.add('active');
-                    title.textContent = item.title;
-                    text.textContent = item.text;
-                    extratext.innerHTML = item.extratext;
-                    wraptext.classList.add(item.color);
-                    innerWraptext.classList.add('transation');
-
-                    if (extratext.textContent == "") {
-                        extratext.classList.add("hide");
-                    } else {
-                        extratext.classList.remove("hide");
-                    }
-
-                    wraptext.style.height = innerWraptext.clientHeight + offset + 'px';
-                    innerWraptext.classList.remove("transation");
-                });
-
-                pin.addEventListener('mouseleave', function () {
-
-
-                    defaultElement.classList.add("active");
-                    this.classList.remove('active');
-                    wraptext.classList.remove(item.color);
-
-                    title.textContent = defaultData.title;
-                    text.textContent = defaultData.text;
-                    extratext.innerHTML = defaultData.extratext;
-
-                    if (extratext.textContent == "") {
-                        extratext.classList.add("hide");
-                    } else {
-                        extratext.classList.remove("hide");
-                    }
-
-                    wraptext.style.height = innerWraptext.clientHeight + 64 + 'px';
-                });
+            if (pin !== defaultElement) {
+                pin.addEventListener('mouseenter', () => handleMouseEnter(item));
+                pin.addEventListener('mouseleave', () => handleMouseLeave(item));
             }
+        });
+    }
 
+    function handleMouseEnter(item) {
+        defaultElement.classList.remove("active");
+        pin.classList.add('active');
+        title.textContent = item.title;
+        text.textContent = item.text;
+        extratext.innerHTML = item.extratext;
+        wraptext.classList.add(item.color);
+        innerWraptext.classList.add('transation');
+
+        if (!extratext.textContent) {
+            extratext.classList.add("hide");
+        } else {
+            extratext.classList.remove("hide");
         }
+
+        wraptext.style.height = `${innerWraptext.clientHeight + offset}px`;
+        innerWraptext.classList.remove("transation");
+    }
+
+    function handleMouseLeave(item) {
+        defaultElement.classList.add("active");
+        pin.classList.remove('active');
+        wraptext.classList.remove(item.color);
+
+        title.textContent = defaultData.title;
+        text.textContent = defaultData.text;
+        extratext.innerHTML = defaultData.extratext;
+
+        if (!extratext.textContent) {
+            extratext.classList.add("hide");
+        } else {
+            extratext.classList.remove("hide");
+        }
+
+        wraptext.style.height = `${innerWraptext.clientHeight + offset}px`;
     }
 
     getData();
